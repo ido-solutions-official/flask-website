b4a29efc118be256f8a59cf425dfc4bb04d39185
Aim <g.ex_aim@gmail.com>
2019-07-07 15:20:26 +0700

    stable

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-# EX location of app.py ==> contain app = Flask(__name__)
-# /home/yourusername/mysite/flask_app.py
-
-import sys
-# path = '/home/yourusername/mysite/flask_app.py'
-if path not in sys.path:
-   sys.path.insert(0, path)
-
-from app import app as application
